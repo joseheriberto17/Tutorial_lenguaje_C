efd1eaa2fa22f37d0b0e118c073110d642b67293
theheri24 <theheri24@theheri24-Aspire-A315-53G>
2023-10-12 22:02:16 -0500

    inicio del proyecto C CDT C/C++

---
+++ src/Tutorial_lenguaje_C.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Tutorial_lenguaje_C.c
+ Author      : jose heriberto marquez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Tutorial"); /* prints Tutorial */
+	return EXIT_SUCCESS;
+}
